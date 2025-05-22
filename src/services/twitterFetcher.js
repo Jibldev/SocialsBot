40d72e47998c6714fa9e929126cce803c5d83657
Antoine Mistral <antoinemistral.dev@gmail.com>
2025-05-22 16:45:29 +0200

    Tweet with image only

--- src/services/twitterFetcher.js
+++ src/services/twitterFetcher.js
@@ -10,19 +10,35 @@ async function getLatestTweet(userId) {
   try {
     const timeline = await twitterClient.v2.userTimeline(userId, {
       exclude: "retweets,replies",
+      expansions: ["attachments.media_keys"],
+      "media.fields": ["type", "url", "preview_image_url"],
+      "tweet.fields": ["created_at", "attachments", "text"],
       max_results: 5,
-      "tweet.fields": "created_at",
     });
 
-    const tweet = timeline.data?.data?.[0];
-    if (!tweet) return null;
+    const tweets = timeline.data?.data;
+    const media = timeline.data?.includes?.media;
 
-    return {
-      id: tweet.id,
-      url: `https://twitter.com/i/web/status/${tweet.id}`,
-      text: tweet.text,
-      date: tweet.created_at,
-    };
+    if (!tweets || !media) return null;
+
+    for (const tweet of tweets) {
+      const mediaKeys = tweet.attachments?.media_keys;
+      if (!mediaKeys) continue;
+
+      const tweetMedia = media.filter((m) => mediaKeys.includes(m.media_key));
+
+      const hasPhoto = tweetMedia.some((m) => m.type === "photo");
+      if (!hasPhoto) continue;
+
+      return {
+        id: tweet.id,
+        url: `https://twitter.com/i/web/status/${tweet.id}`,
+        text: tweet.text,
+        date: tweet.created_at,
+      };
+    }
+
+    return null; // Aucun tweet avec image
   } catch (err) {
     console.error("[Twitter Fetcher] Erreur :", err);
     return null;
